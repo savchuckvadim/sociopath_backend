2555b441042e26cf6d5e45d36ad04852aea6aa95
savchuckvadim <savchuckvadim@gmail.com>
2022-09-01 16:07:40 +0300

    fixed websocket prefences

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,9 +35,16 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
+            // 'options' => [
+            //     'cluster' => env('PUSHER_APP_CLUSTER'),
+            //     'useTLS' => true,
+            // ],
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
